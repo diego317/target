c0dd95a6b243b4ace279c3dd8637cbe03967299d
Diego Celis <c.e.l.is@hotmail.com>
2022-03-30 15:21:51 -0500

    add i18next

--- src/i18n/locales/en/index.ts
+++ src/i18n/locales/en/index.ts
@@ -1,11 +1,11 @@
 import common from './common.json';
-import errors from './errors.json';
+import errors from './errors.json'
 import signUp from './signUp.json';
 
 const resource = {
   common,
   errors,
-  signUp,
+  signUp, 
 };
 
 export default resource;
